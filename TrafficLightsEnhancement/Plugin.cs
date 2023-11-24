b519a95cb46891fc0d1766cb07dba8a996c84abd
SuperYYT <1209897320@qq.com>
2023-11-24 10:55:44 +0800

    Update Plugin.cs
    汉化

--- TrafficLightsEnhancement/Plugin.cs
+++ TrafficLightsEnhancement/Plugin.cs
@@ -4,12 +4,12 @@ using HarmonyLib;
 
 namespace C2VM.TrafficLightsEnhancement;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)] 
 public class Plugin : BaseUnityPlugin
 {
     private void Awake()
     {
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+        Logger.LogInfo($"插件 {MyPluginInfo.PLUGIN_GUID} {MyPluginInfo.PLUGIN_VERSION} 成功加载！"); 
 
         var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
         harmony.PatchAll();
